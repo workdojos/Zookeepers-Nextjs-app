222df15c0395695ca558c59b26a5f7af15b2b6d8
viewprox86 <workmates.live@outlook.com>
2023-10-09 13:05:42 -0500

    s

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -43,17 +43,17 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="https://meet.admins.live/join?room=Kevin+Bacon&name=Guest"
+              href="https://blog.workdojos.com/#/portal/signup"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Join
+              Join for Free
             </Link>
             <Link
-              href="mailto:support@workmates.live"
+              href="mailto:jay@workmates.live"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                E-mail Us
+                E-mail Me
               </span>
             </Link>
           </div>
