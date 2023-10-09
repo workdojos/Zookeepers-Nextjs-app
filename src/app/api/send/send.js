b5a5761be4da7c95aa14035f8800c7d4e3a48f89
viewprox86 <workmates.live@outlook.com>
2023-10-09 13:51:27 -0500

    s

---
+++ src/app/api/send/send.js
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <admin@workmates.live',
+      to: ['admin@workmates.live'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
