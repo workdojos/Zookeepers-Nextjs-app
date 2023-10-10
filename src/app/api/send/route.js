4365b1f8b0351e30e5b8d7a3462f2b3649c4c9d2
viewprox86 <workmates.live@outlook.com>
2023-10-10 16:14:51 -0500

    as

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -6,23 +6,22 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = req;
-  const { email, subject, message } = body;
-  console.log(body);
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["apozitude@gmail.com", email],
+      to: [fromEmail, email],
       subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Got it!  Reply to this e-mail if you have any questions.  Thanks!</p>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
           <p>{message}</p>
         </>
       ),
     });
-
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
