49e68739595961f51cf705855a2e428d982c9a20
Workdojos <36578874+workdojos@users.noreply.github.com>
2024-03-24 10:13:56 -0700

    Update base.js

--- src/app/api/base.js
+++ src/app/api/base.js
@@ -1,7 +1,7 @@
 import Airtable from 'airtable';
 
 // api keys are confidential
-const base = new Airtable({ apiKey: 'pat3EZwRNaBA5XnC9.391f73ce2b4b3d1793506cd30e7162ebeed69d259e5ebf006df06cee19f92c07' }).base(
+const base = new Airtable({ apiKey: '' }).base(
   'Primary'
 );
 
